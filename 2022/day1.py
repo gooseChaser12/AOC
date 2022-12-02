93a1eee9f9f546bc047162e9ad3b8bca1f0ae63c
gooseChaser12 <matthew.bourque116@gmail.com>
2022-12-02 00:39:53 -0500

    day 2

---
+++ 2022/day1.py
@@ -0,0 +1,63 @@
+def max_cals(lines):
+    '''
+    Calculates the elf with the most calories.
+    '''
+    max = 0  # Storing max cal for each elf
+    cur_elf = 0  # Storing cal for current elf
+    # Iterating through the lines
+    for line in lines:
+        if line == "\n":
+            # Update max value if necessary
+            if cur_elf > max:
+                max = cur_elf
+            # Reset current cal counter
+            cur_elf = 0
+        else:
+            cur_cal = int(line)
+            cur_elf = cur_elf + cur_cal
+    return max
+
+
+def sum_list(l):
+    '''
+    Calculates the sum of all elements in a list.
+    '''
+    total = 0
+    for elt in l:
+        total = total + elt
+    return total
+
+
+def top_three(lines):
+    '''
+    Calculates the sum of the top three caloric elves.
+    '''
+    max_calories = [0, 0, 0]  # Storing top three cals
+    cur_elf = 0
+    # Iterating through the lines
+    for line in lines:
+        if line == "\n":
+            if cur_elf >= max_calories[0]:
+                max_calories[0] = cur_elf
+                # Sorting the list because it's only 3 elements
+                # Guarantees smallest element is at front
+                max_calories.sort()
+            cur_elf = 0
+        else:
+            cur_cal = int(line)
+            cur_elf = cur_elf + cur_cal
+    return sum_list(max_calories)
+
+
+def main():
+    try:
+        f = open("day1.txt", mode='r', encoding='utf-8')
+        lines = f.readlines()
+        print("ANS 1 : ", max_cals(lines))
+        print("ANS 2 : ", top_three(lines))
+        f.close()
+    except (FileNotFoundError):
+        print("Can't open file")
+
+
+main()
---
+++ 2022/day2.py
@@ -0,0 +1,101 @@
+def rps_round(p1, p2):
+    '''
+    Calculating score for player 1
+    '''
+    # You choose rock
+    if p1 == 'X':
+        # They choose rock
+        if p2 == 'A':
+            return 1 + 3
+        # They choose paper
+        elif p2 == 'B':
+            return 1
+        # They choose scissor
+        return 1 + 6
+    # You choose paper
+    elif p1 == 'Y':
+        # They choose rock
+        if p2 == 'A':
+            return 2 + 6
+        # They choose paper
+        elif p2 == 'B':
+            return 2 + 3
+        # They choose scissor
+        return 2
+    # You choose scissor
+    elif p1 == 'Z':
+        # They choose rock
+        if p2 == 'A':
+            return 3
+        # They choose paper
+        elif p2 == 'B':
+            return 3 + 6
+        # They choose scissor
+    return 3 + 3
+
+def rps_score(lines):
+    score = 0
+    for line in lines:
+        guide = line.split()
+        p2 = guide[0]
+        p1 = guide[1]
+        score = score + rps_round(p1, p2)
+    return score
+
+
+def rps_round_end(p1, p2):
+    # If they choose rock
+    if p2 == 'A':
+        # Lose with a scissor
+        if p1 == 'X':
+            return rps_round('Z', p2)
+        # Draw with a rock
+        if p1 == 'Y':
+            return rps_round('X', p2)
+        # Win with a paper
+        return rps_round('Y', p2)
+    # If they choose paper
+    elif p2 == 'B':
+        # Lose with a rock
+        if p1 == 'X':
+            return rps_round('X', p2)
+        # Draw with a paper
+        if p1 == 'Y':
+            return rps_round('Y', p2)
+        # Win with a scissor
+        return rps_round('Z', p2)
+    # If they choose scissor
+    # Lose with a paper
+    if p1 == 'X':
+        return rps_round('Y', p2)
+    # Draw with a scissor
+    if p1 == 'Y':
+        return rps_round('Z', p2)
+    # Win with a rock
+    return rps_round('X', p2)
+
+
+def rps_end_score(lines):
+    score = 0
+    for line in lines:
+        guide = line.split()
+        p1 = guide[1]
+        p2 = guide[0]
+        score = score + rps_round_end(p1, p2)
+    return score
+
+
+def main():
+    try:
+        f = open("day2.txt", mode='r', encoding='utf-8')
+        lines = f.readlines()
+        print("ANS 1 : ", rps_score(lines))
+        print("ANS 2 : ", rps_end_score(lines))
+        f.close()
+    except (FileNotFoundError):
+        print("Can't open file")
+
+
+main()
+
+# Damn you python and you no switch statements
